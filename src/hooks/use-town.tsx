4ba7095855c9abb7ad7e0bcfb4b492d71039a36e
Miro Grujin <miro.grujin@hotmail.com>
2025-07-28 11:59:03 +0200

    update: reorder towns so wien is the first option

--- src/hooks/use-town.tsx
+++ src/hooks/use-town.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { AdditionalaAndTownsModel } from '@/types/additionals';
 import { TownModel } from '@/types/town';
 import { useAtom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
-import { AdditionalaAndTownsModel } from '@/types/additionals';
+import { useState } from 'react';
 
 export const orderAdditionalsAtom = atomWithStorage<any>('additionals', {});
 
@@ -14,23 +14,35 @@ const useTown = () => {
   const [towns, setTowns] = useState<string[]>();
   const [selectedTown, setSelectedTown] = useState<string>();
 
-  const getTownsAndDistricts = async (): Promise<TownModel[]> => {
-    const dbData = await fetch(`${import.meta.env.VITE_APP_API_URL}/towns/all`)
-      .then((response) => response.json())
-      .then((data) => {
-        setAdditionalsAndTowns({
-          additionals: data.additionals[0],
-          towns: data.object,
-        });
-        return data.object;
-      })
-      .catch(console.error);
-    handleTowns(dbData);
+  const getTownsAndDistricts = async (): Promise<TownModel> => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_APP_API_URL}/towns/all`
+      );
+      const data = await response.json();
 
-    return dbData;
+      // reorder the raw object to have 'Wien' as the first key
+      const towns: TownModel = {
+        Wien: data.object['Wien'],
+        ...Object.fromEntries(
+          Object.entries(data.object).filter(([key]) => key !== 'Wien')
+        ),
+      };
+
+      setAdditionalsAndTowns({
+        additionals: data.additionals[0],
+        towns: towns,
+      });
+
+      handleTowns(towns);
+      return towns;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   };
 
-  const handleTowns = (towns: TownModel[]) => {
+  const handleTowns = (towns: TownModel) => {
     const parsedTowns = Object.keys(towns).map((town) => town);
     setTowns(parsedTowns);
     setSelectedTown(parsedTowns[0]);
